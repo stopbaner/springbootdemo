ea085ec0a69d3fb2a700efdc0f55e3624d552513
stopbaner <97634674@qq.com>
2021-12-20 16:36:55 +0800

    update

--- src/main/java/com/shenzp/springbootdemo/springboottest/SpringbootdemoApplication.java
+++ src/main/java/com/shenzp/springbootdemo/springboottest/SpringbootdemoApplication.java
@@ -11,19 +11,21 @@ import javax.annotation.PreDestroy;
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
-    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
+        //第二种启动方式(web方式)
+        //new SpringApplicationBuilder().main(SpringbootdemoApplication.class).web(WebApplicationType.NONE).build(args);
     }
 
     @PostConstruct
-    public void afterInit(){
+    public void afterInit() {
         logger.info("构造方法与autowired执行完毕,servlet的init方法未执行");
     }
 
     @PreDestroy
-    public void destroy(){
+    public void destroy() {
         logger.info("servlet的destroy执行完毕");
     }
 
