191999bf9788dc99b6d0f3a2404f6f985460812f
stopbaner <97634674@qq.com>
2021-12-20 18:11:44 +0800

    inteceptor

---
+++ src/main/java/com/shenzp/springbootdemo/springboottest/intecerptor/MyInterceptor.java
@@ -0,0 +1,29 @@
+package com.shenzp.springbootdemo.springboottest.intecerptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+    }
+}
