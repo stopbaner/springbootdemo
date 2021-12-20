7673da3b2299d1c35270370cc80a1019a875a2ef
stopbaner <97634674@qq.com>
2021-12-20 15:43:09 +0800

    preDestroy

--- src/main/java/com/shenzp/springbootdemo/springboottest/SpringbootdemoApplication.java
+++ src/main/java/com/shenzp/springbootdemo/springboottest/SpringbootdemoApplication.java
@@ -1,13 +1,30 @@
 package com.shenzp.springbootdemo.springboottest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void afterInit(){
+        logger.info("构造方法与autowired执行完毕,servlet的init方法未执行");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        logger.info("servlet的destroy执行完毕");
     }
 
 }
